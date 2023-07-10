9ca8b91bb688a8fa8ffac76fc78204b69891dc10
Thaila Marani <thaila_marani@hotmail.com>
2023-07-10 16:50:14 -0300

    Parte 1

---
+++ src/errors/BadRequestError.ts
@@ -0,0 +1,9 @@
+import { BaseError } from './BaseError';
+
+export class BadRequestError extends BaseError {
+    constructor(
+        message: string = "Dados inválidos"
+    ) {
+        super(400, message)
+    }
+};
