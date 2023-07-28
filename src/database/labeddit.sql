1a7638ac76737e8945f315bd4422298602442e05
Thaila Marani <thaila_marani@hotmail.com>
2023-07-28 15:36:01 -0300

    Organizando

--- src/database/labeddit.sql
+++ src/database/labeddit.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE
     users(
-        user_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+        id TEXT PRIMARY KEY NOT NULL UNIQUE,
         nickname TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
@@ -10,25 +10,25 @@ CREATE TABLE
 
 CREATE TABLE
     posts(
-        post_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+        id TEXT PRIMARY KEY NOT NULL UNIQUE,
         creator_id TEXT NOT NULL,
         content TEXT NOT NULL,
         votes_count INTEGER NOT NULL,
         comments_count INTEGER NOT NULL,
         created_at TEXT NOT NULL,
-        FOREIGN KEY (creator_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (creator_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
     );
 
 CREATE TABLE
     comments(
-        comment_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+        id TEXT PRIMARY KEY NOT NULL UNIQUE,
         post_id TEXT NOT NULL,
         creator_id TEXT NOT NULL,
         content TEXT NOT NULL,
         votes_count INTEGER NOT NULL,
         created_at TEXT NOT NULL,
-        FOREIGN KEY (post_id) REFERENCES posts(post_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (creator_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (creator_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
     );
 
 CREATE TABLE
@@ -36,8 +36,8 @@ CREATE TABLE
         post_id TEXT NOT NULL,
         user_id TEXT NOT NULL,
         vote INTEGER NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (post_id) REFERENCES posts(post_id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE
     );
 
 CREATE TABLE
@@ -45,8 +45,8 @@ CREATE TABLE
         comment_id TEXT NOT NULL,
         user_id TEXT NOT NULL,
         vote INTEGER NOT NULL,
-        FOREIGN KEY (comment_id) REFERENCES comments (comment_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (comment_id) REFERENCES comments (id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
     );
 
 DROP TABLE users;
